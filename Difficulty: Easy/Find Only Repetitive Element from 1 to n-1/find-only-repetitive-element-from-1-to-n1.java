1181062f66ed415cb6bd36b4f8a66a77f7d110d9
PREM KUMAR GUPTA <premgupta100705@gmail.com>
2026-07-04 11:59:07 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Find Only Repetitive Element from 1 to n-1/find-only-repetitive-element-from-1-to-n1.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int findDuplicate(int[] nums) {
+        // code here
+        int slow = nums[0];
+        int fast = nums[0];
+        // pehle aapn check krte h duplicate exist krta h y nhi
+         do {
+            slow = nums[slow];  // ek ko 1 step 
+            fast = nums[nums[fast]]; // doosre ko 2 step
+        } while (slow != fast);
+        // to ek ko starting point p krte h 
+        // doosre ko meeting point p 
+        slow = nums[0];
+         while(slow != fast){
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+
+        return slow;
+    }
+}
