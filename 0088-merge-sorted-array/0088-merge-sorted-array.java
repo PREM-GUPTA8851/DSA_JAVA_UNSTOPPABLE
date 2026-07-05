554d7f537aee4e9002447753e108ab3460c60140
PREM KUMAR GUPTA <premgupta100705@gmail.com>
2026-07-05 14:14:26 +0530

    Time: 0 ms (100%), Space: 43.8 MB (61.39%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,22 +1,32 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-
-        int i = m - 1;        // nums1 ke last valid element par
-        int j = n - 1;        // nums2 ke last element par
-        int k = m + n - 1;    // nums1 ke last index par
-
+        //  nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
+        int i = m - 1;        // 2
+        int j = n - 1;        // 2
+        int k = m + n - 1;    // 5
         // Back se merge karo
         while (i >= 0 && j >= 0) {
-
+        // nums1[2] = 3 > nums2[2] = 6 --> else part
             if (nums1[i] > nums2[j]) {
                 nums1[k] = nums1[i];
+            // nums1[3] = nums1[2] = 3
+            // nums1 = [1,2,3 ,3,5,6]
                 i--;
+                // i = 1
             } else {
                 nums1[k] = nums2[j];
+                // nums1[5] = nums2[2] = 6
+                // nums1[4] = 5;
+                // num1[2] = 2;
                 j--;
+                // j = 1
+                // j = 0;
             }
 
             k--;
+            // k = 4
+            // k = 3;
+            // k = 2
         }
 
         // Agar nums2 me elements bach gaye
