bb927cb70f5e05e375b145868795f6d48a3c774d
PREM KUMAR GUPTA <premgupta100705@gmail.com>
2026-07-05 23:40:55 +0530

    Time: 8 ms (61.93%), Space: 43.1 MB (54.31%) - LeetHub

--- 0038-count-and-say/0038-count-and-say.java
+++ 0038-count-and-say/0038-count-and-say.java
@@ -6,21 +6,26 @@ class Solution {
         // Generate sequence from 2 to n
         // 2,3,4
         for (int i = 2; i <= n; i++) {
-
+        // i = 2
+        // i = 3
+        // i = 4
             StringBuilder temp = new StringBuilder();
-
+            // temp = "";
             int count = 1;
 
             // Traverse previous string
-            for (int j = 1; j < ans.length(); j++) {
+            // ans.length = 1
+            // ans.length = 2
 
-                if (ans.charAt(j) == ans.charAt(j - 1)) {
-                    count++;
+        for (int j = 1; j < ans.length(); j++) {
+
+            if(ans.charAt(j) == ans.charAt(j - 1)) {
+                    count++; // count = 2
                 } else {
 
                     // Append frequency + character
-                    temp.append(count);
-                    temp.append(ans.charAt(j - 1));
+                    temp.append(count); // 1
+                    temp.append(ans.charAt(j - 1)); 
 
                     count = 1;
                 }
@@ -28,9 +33,14 @@ class Solution {
 
             // Last group
             temp.append(count);
+            // temp = 1
+            // temp.append(2);
             temp.append(ans.charAt(ans.length() - 1));
-
+            // 11
+            // temp.append(1)
             ans = temp.toString();
+            // ans = 11
+            // ans = 21
         }
 
         return ans;
