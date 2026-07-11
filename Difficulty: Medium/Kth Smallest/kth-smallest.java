3741de6dbfc7a97cbe19e3eb5158a67a15e6fefd
PREM KUMAR GUPTA <premgupta100705@gmail.com>
2026-07-11 16:53:46 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Kth Smallest/kth-smallest.java
+++ Difficulty: Medium/Kth Smallest/kth-smallest.java
@@ -1,6 +1,6 @@
 class Solution {
     public int kthSmallest(int[] arr, int k) {
-        // Code here
+        // firstly sort then return k-1th element
         Arrays.sort(arr);
         return arr[k-1];
     }
