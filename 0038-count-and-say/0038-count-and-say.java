7317d900b7e816726b094f06ca54bb923a9b0ca4
PREM KUMAR GUPTA <premgupta100705@gmail.com>
2026-07-13 19:05:20 +0530

    Time: 8 ms (61.92%), Space: 42.8 MB (83.86%) - LeetHub

--- 0038-count-and-say/0038-count-and-say.java
+++ 0038-count-and-say/0038-count-and-say.java
@@ -2,31 +2,43 @@ class Solution {
     public String countAndSay(int n) {
         String ans = "1"; // Base case
         // Generate sequence from 2 to n
-        // 2,3,4
+        // 2,3,4,5
         for (int i = 2; i <= n; i++) {
         // i = 2
         // i = 3
         // i = 4
-            StringBuilder temp = new StringBuilder();
-            int count = 1;
+        // i = 5
+        StringBuilder temp = new StringBuilder();
+        int count = 1;
         // ans --> 1 --> length = 1
         // ans --> 11 --> length = 2
         // ans --> 21 --> length = 2
+        // ans --> 1211 --> length = 4
+
 
         // 2nd one --> 1 < 2 --> ..
         // 3rd one --> 1 < 2-->...
+        // 4th one --> 1 < 4
     for (int j = 1; j < ans.length(); j++) {
-        
+        // 4th one --> 1 < 4
+        // 2 < 4
+        // 3 < 4
         if(ans.charAt(j) == ans.charAt(j - 1)) {
             count++; 
-            
+            // 2
         // count = 2
         } else {
                     temp.append(count); 
                     // temp.append(1) --> temp --> 1
+                    // temp.append(1);
+                    // temp.append(1);
                     temp.append(ans.charAt(j - 1)); 
                     // temp.append(ans.charAt(0)) --> 
                     // temp.append(2) --> temp --> 12
+                    // temp.append(1)
+                    // temp.append(2)
+
+                    /// temp --> 1112
                     count = 1;
                 }
             }
@@ -36,14 +48,18 @@ class Solution {
             // temp.append(1) --> temp --> 1
             // temp.append(2) --> 2
             // temp.append(1) --> 121
+            // temp.append(2)
             temp.append(ans.charAt(ans.length() - 1));
             // temp.append(ans.charAt(0)) --> temp.append(1)
             // temp.append(ans.charAt(1)) --> temp.append(1)
             // temp.append(1) --> temp --> 1211
+            // temp.append(1)
 
+            // temp--> 111211
             ans = temp.toString();
             // ans = "11"
             // ans = "21"
+            // ans = "111211"
 
         }
 
