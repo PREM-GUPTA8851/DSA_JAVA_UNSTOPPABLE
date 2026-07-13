7a284d4b605415dc58a7b8e1238e48831489a369
PREM KUMAR GUPTA <premgupta100705@gmail.com>
2026-07-13 13:36:46 +0530

    Time: 4 ms (90.75%), Space: 82.8 MB (69.4%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
@@ -2,23 +2,34 @@ class Solution {
     public int findDuplicate(int[] nums) {
 
         // by using floyd's cycle detection algorithm
-        
+        // dry run --> [1,3,4,2,2]
+        // DRY RUN --> [3,1,3,4,2]
         // firstly we check duplicate exist or not
-        int slow = nums[0];
-        int fast = nums[0];
+        int slow = nums[0]; // 1 
+        // 3 
+        int fast = nums[0]; // 1
+        // 3
         // pehle aapn check krte h duplicate exist krta h y nhi
-         do {
-            slow = nums[slow];  // ek ko 1 step 
+        do {
+            slow = nums[slow];  // ek ko 1 step  
+            // 3, 2
+            // 4, 2, 3
             fast = nums[nums[fast]]; // doosre ko 2 step
+            //2, 2
+            // 2, 4, 3
         } while (slow != fast);
         // to ek ko starting point p krte h 
         // doosre ko meeting point p 
-        slow = nums[0];
+        slow = nums[0]; // 1
+        // 3
          while(slow != fast){
-            slow = nums[slow];
+            slow = nums[slow]; 
+            // 3 2
             fast = nums[fast];
+            // 4 2
         }
 
-        return slow;
+        return slow; // 2
+        // 3
     }
 }
