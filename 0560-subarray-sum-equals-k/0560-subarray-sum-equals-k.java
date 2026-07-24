5ab3a8d1e1089c9b35461c20eca3555aa9c85b6e
PREM KUMAR GUPTA <premgupta100705@gmail.com>
2026-07-24 17:53:20 +0530

    Time: 25 ms (46.69%), Space: 48.8 MB (59.18%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
@@ -3,7 +3,7 @@ class Solution {
 
         HashMap<Integer, Integer> map = new HashMap<>();
 
-        map.put(0, 1); // Empty prefix
+        map.put(0, 1); // map = {0=1}
 
         int sum = 0;
         int count = 0;
@@ -11,6 +11,75 @@ class Solution {
         for (int num : nums) {
 
             sum += num;
+
+            /*
+            Dry Run
+
+            nums = [1, 2, 3]
+            k = 3
+
+            -------------------------
+            Iteration 1
+            num = 1
+
+            sum = 0 + 1 = 1
+
+            sum - k = 1 - 3 = -2
+
+            map = {0=1}
+
+            map.containsKey(-2) -> false
+
+            count = 0
+
+            map.put(1,1)
+
+            map = {0=1, 1=1}
+
+            -------------------------
+            Iteration 2
+            num = 2
+
+            sum = 1 + 2 = 3
+
+            sum - k = 3 - 3 = 0
+
+            map = {0=1, 1=1}
+
+            map.containsKey(0) -> true
+
+            count += map.get(0)
+
+            count = 0 + 1 = 1
+
+            map.put(3,1)
+
+            map = {0=1, 1=1, 3=1}
+
+            -------------------------
+            Iteration 3
+            num = 3
+
+            sum = 3 + 3 = 6
+
+            sum - k = 6 - 3 = 3
+
+            map = {0=1, 1=1, 3=1}
+
+            map.containsKey(3) -> true
+
+            count += map.get(3)
+
+            count = 1 + 1 = 2
+
+            map.put(6,1)
+
+            map = {0=1, 1=1, 3=1, 6=1}
+
+            Loop End
+
+            return 2
+            */
 
             if (map.containsKey(sum - k)) {
                 count += map.get(sum - k);
