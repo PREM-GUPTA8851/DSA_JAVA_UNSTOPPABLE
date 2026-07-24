2d82d99941031748689daef80e5c25f3f973abdb
PREM KUMAR GUPTA <premgupta100705@gmail.com>
2026-07-24 19:07:33 +0530

    Time: 24 ms (77.03%), Space: 48.9 MB (45.99%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
@@ -1,93 +1,75 @@
 class Solution {
     public int subarraySum(int[] nums, int k) {
 
-        HashMap<Integer, Integer> map = new HashMap<>();
+    HashMap<Integer, Integer> map = new HashMap<>();
+        // Prefix Sum aur uski frequency store karega.
 
-        map.put(0, 1); // map = {0=1}
+        map.put(0, 1);
+        // Shuru me sum = 0 ek baar aaya hai.
+        // map = {0=1}
 
         int sum = 0;
+        // Current Prefix Sum
+
         int count = 0;
+        // Answer store karega
 
         for (int num : nums) {
+        // nums = [1,2,3], k = 3
 
             sum += num;
-
-            /*
-            Dry Run
-
-            nums = [1, 2, 3]
-            k = 3
-
-            -------------------------
-            Iteration 1
-            num = 1
-
-            sum = 0 + 1 = 1
-
-            sum - k = 1 - 3 = -2
-
-            map = {0=1}
-
-            map.containsKey(-2) -> false
-
-            count = 0
-
-            map.put(1,1)
-
-            map = {0=1, 1=1}
-
-            -------------------------
-            Iteration 2
-            num = 2
-
-            sum = 1 + 2 = 3
-
-            sum - k = 3 - 3 = 0
-
-            map = {0=1, 1=1}
-
-            map.containsKey(0) -> true
-
-            count += map.get(0)
-
-            count = 0 + 1 = 1
-
-            map.put(3,1)
-
-            map = {0=1, 1=1, 3=1}
-
-            -------------------------
-            Iteration 3
-            num = 3
-
-            sum = 3 + 3 = 6
-
-            sum - k = 6 - 3 = 3
-
-            map = {0=1, 1=1, 3=1}
-
-            map.containsKey(3) -> true
-
-            count += map.get(3)
-
-            count = 1 + 1 = 2
-
-            map.put(6,1)
-
-            map = {0=1, 1=1, 3=1, 6=1}
-
-            Loop End
-
-            return 2
-            */
+            // -------------------------
+            // Iteration 1
+            // num = 1
+            // sum = 0 + 1 = 1
+            //
+            // Iteration 2
+            // num = 2
+            // sum = 1 + 2 = 3
+            //
+            // Iteration 3
+            // num = 3
+            // sum = 3 + 3 = 6
 
             if (map.containsKey(sum - k)) {
+            // sum-k check karte hain.
+            //
+            // Iteration 1
+            // sum-k = 1-3 = -2
+            // map me -2 nhi hai.
+            //
+            // Iteration 2
+            // sum-k = 3-3 = 0
+            // map me 0 hai.
+            // Matlab yaha tak koi subarray ka sum = 3 mila.
+            //
+            // count += map.get(0)
+            // count = 1
+            //
+            // Iteration 3
+            // sum-k = 6-3 = 3
+            // map me 3 hai.
+            // Matlab ek aur subarray ka sum = 3 mila.
+            // count = 2
+
                 count += map.get(sum - k);
+                // Frequency jitni hogi utne subarray milenge.
             }
 
             map.put(sum, map.getOrDefault(sum, 0) + 1);
+            // Current Prefix Sum ko map me store kar do.
+            //
+            // Iteration 1
+            // map = {0=1,1=1}
+            //
+            // Iteration 2
+            // map = {0=1,1=1,3=1}
+            //
+            // Iteration 3
+            // map = {0=1,1=1,3=1,6=1}
         }
 
         return count;
+        // Final Answer = 2
     }
 }
