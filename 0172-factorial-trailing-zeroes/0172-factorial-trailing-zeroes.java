991876fdb3900d41e3eb1021f9a22511070f1aa9
PREM KUMAR GUPTA <premgupta100705@gmail.com>
2026-07-30 19:47:42 +0530

    Time: 0 ms (100%), Space: 42.6 MB (17.34%) - LeetHub

--- 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.java
+++ 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.java
@@ -1,11 +1,36 @@
 class Solution {
     public int trailingZeroes(int n) {
-        // 100/5 --> 20 + 20 /5 --> 20 + 4/5--> 0. --> 24
+        
+        // n = 100
+        // count = 0
+
+        // count += 100 / 5
+        // count = 20
+
+        // n = 100 / 5
+        // n = 20
+
+        // count += 20 / 5
+        // count = 24
+
+        // n = 20 / 5
+        // n = 4
+
+        // count += 4 / 5
+        // count = 24
+
+        // n = 4 / 5
+        // n = 0
+
+        //  24
+
         int count = 0;
+
         while(n > 0){
             count += n / 5;
             n = n / 5;
         }
+
         return count;
     }
 }
