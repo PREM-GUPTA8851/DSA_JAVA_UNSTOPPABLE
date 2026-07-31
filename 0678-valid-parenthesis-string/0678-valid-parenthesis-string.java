90e26a3343c42b2255633f9ba6c4b483b44bf36e
PREM KUMAR GUPTA <premgupta100705@gmail.com>
2026-07-31 18:10:31 +0530

    Time: 0 ms (100%), Space: 42.6 MB (84.72%) - LeetHub

---
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.java
@@ -0,0 +1,76 @@
+class Solution {
+    public boolean checkValidString(String s) {
+
+        // Dry Run
+        // Input : "(*))"
+
+        // low = Minimum possible open brackets
+        // high = Maximum possible open brackets
+
+        int low = 0;
+        int high = 0;
+
+        for(int i = 0; i < s.length(); i++){
+
+            // i = 0
+            // s.charAt(0) = '('
+            // low = 1
+            // high = 1
+
+            // ------------------------
+
+            // i = 1
+            // s.charAt(1) = '*'
+            // '*' -> ')' => low--
+            // '*' -> '(' => high++
+            // low = 0
+            // high = 2
+
+            // ------------------------
+
+            // i = 2
+            // s.charAt(2) = ')'
+            // low--
+            // high--
+            // low = -1 -> 0
+            // high = 1
+
+            // ------------------------
+
+            // i = 3
+            // s.charAt(3) = ')'
+            // low--
+            // high--
+            // low = -1 -> 0
+            // high = 0
+
+            // ------------------------
+            // End
+            // low == 0
+            // Return true
+
+            char ch = s.charAt(i);
+
+            if(ch == '('){
+                low++;
+                high++;
+            }
+            else if(ch == ')'){
+                low--;
+                high--;
+            }
+            else{ // '*'
+                low--;      // '*' as ')'
+                high++;     // '*' as '('
+            }
+
+            if(low < 0)
+                low = 0;
+
+            if(high < 0)
+                return false;
+        }
+
+        return low == 0;
+    }
+}
