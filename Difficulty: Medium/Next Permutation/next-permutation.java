c54abb2ef0e461c432533b141424702b394dcf7f
PREM KUMAR GUPTA <premgupta100705@gmail.com>
2026-08-02 17:30:10 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Next Permutation/next-permutation.java
+++ Difficulty: Medium/Next Permutation/next-permutation.java
@@ -1,46 +1,76 @@
 class Solution {
     void nextPermutation(int[] arr) {
-    // lexicographical next permutations algorithm
-    // first step we need to find the pivot 
-    /*
-    pivot --> right se jha p element chota
-    mile next element se
-    */
-    int n = arr.length;
-    int pivot = -1;
-    for(int i = n-2; i >=0; i--){
-        if(arr[i] < arr[i + 1]){
-            pivot = i;
-            break;
+
+        // Dry Run
+        // Input : [1, 3, 2]
+
+        int n = arr.length;
+        int pivot = -1;
+
+        // Pivot Find
+        for(int i = n - 2; i >= 0; i--){
+
+            // i = 1
+            // 3 < 2  -> No
+
+            // i = 0
+            // 1 < 3  -> Yes
+            // pivot = 0
+
+            if(arr[i] < arr[i + 1]){
+                pivot = i;
+                break;
+            }
         }
-    }
-// jb pivot mil jae 
-    if(pivot != -1){
-    // ab pivot mil gya to pivot se just bda
-    //element find krna h pivot index tk aur dono sath m swap kr dena h
-    for(int i = n -1; i > pivot; i--){
-        if(arr[i] > arr[pivot]){
-            // now swap 
-            int temp = arr[i];
-            arr[i] = arr[pivot];
-            arr[pivot] = temp;
-            break;
+
+        // pivot = 0
+
+        if(pivot == -1){
+            reverse(arr,0,n-1);
+            return;
         }
+
+        // Next Greater Element
+        for(int i = n - 1; i > pivot; i--){
+
+            // i = 2
+            // arr[2] = 2
+            // arr[pivot] = 1
+            // 2 > 1
+
+            // Swap
+            // [2,3,1]
+
+            if(arr[i] > arr[pivot]){
+                swap(arr,i,pivot);
+                break;
+            }
+        }
+
+        // Reverse pivot+1 to end
+
+        // Before Reverse
+        // [2,3,1]
+
+        // Reverse (1...2)
+
+        // After Reverse
+        // [2,1,3]
+
+        reverse(arr,pivot+1,n-1);
     }
+
+    void swap(int[] arr,int i,int j){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
-    // now arr = [2,4,5,7,1,0]
-    int left = pivot + 1;
-    int right = n - 1;
-    // right side k pura descending order m h 
-    // aur hume next smallest no. chahiye to reverse kr dena better h
-    // baki remaining swap kr dena
-    while(left < right){
-        int temp = arr[left];
-        arr[left] = arr[right];
-        arr[right] = temp;
-        left++;
-        right--;
-    }
-    // [2,4,5,0,1,7]
+
+    void reverse(int[] arr,int start,int end){
+        while(start < end){
+            swap(arr,start,end);
+            start++;
+            end--;
+        }
     }
 }
