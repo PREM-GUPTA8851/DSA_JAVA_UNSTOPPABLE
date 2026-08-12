835748d971beb3dde48da8c9f32e81ffdf64038c
PREM KUMAR GUPTA <premgupta100705@gmail.com>
2026-08-13 00:17:16 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Longest Common Prefix of Strings/longest-common-prefix-of-strings.java
+++ Difficulty: Easy/Longest Common Prefix of Strings/longest-common-prefix-of-strings.java
@@ -1,29 +1,15 @@
 class Solution {
-    public String longestCommonPrefix(String strs[]) {
-        // code here
- 
-        if (strs == null || strs.length == 0) {
-            return "";
-        }
-
-        // First string ko prefix maan lo
-        String prefix = strs[0];
-
-        // Baaki sab strings ke saath compare karo
-        for (int i = 1; i < strs.length; i++) {
-
-            // Jab tak current string prefix se start nahi hoti,
-            // prefix ka last character remove karte raho
-            while (!strs[i].startsWith(prefix)) {
+    public String longestCommonPrefix(String arr[]) {
+    // logic is pehle aapn ek word pkd lete h then baki check krte jaate h 
+    // ki baki word us first word se start hote h y nhi 
+    // jb tk n ho rha tb tk prefix wali length ko aapn small krte jaenge.
+        String prefix = arr[0]; // geeksforgeeks
+        
+        for(int i = 1; i < arr.length; i++){
+            while(!arr[i].startsWith(prefix)){
                 prefix = prefix.substring(0, prefix.length() - 1);
-
-                // Agar prefix empty ho gaya
-                if (prefix.isEmpty()) {
-                    return "";
-                }
             }
         }
-
         return prefix;
     }
 }
