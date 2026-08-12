49389dc23923b28e1bc6f098aa02169767783e86
PREM KUMAR GUPTA <premgupta100705@gmail.com>
2026-08-12 23:18:48 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Roman to Integer/roman-to-integer.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int romanToInteger(String s) {
+             int ans = 0;
+// MCMXCIV
+        for(int i = 0; i < s.length(); i++){
+            char left = s.charAt(i); 
+        // main logic is left wala chota ho right wale se to ans m subtract kr denge 
+        if(i + 1 < s.length() && value(left) < value(s.charAt(i + 1)))
+        ans -= value(left);
+        // aur agar aesa nhi h (right wala chota h left wale se to ans m add kr denge)
+        else 
+            ans += value(left);
+        }
+        return ans;
+    }
+    int value(char ch){
+            if(ch == 'I') return 1;
+            if(ch == 'V') return 5;
+            if(ch == 'X') return 10;
+            if(ch == 'L') return 50;
+            if(ch == 'C') return 100;
+            if(ch == 'D') return 500;
+            if(ch == 'M') return 1000;
+
+        return 0;
+    }
+}
