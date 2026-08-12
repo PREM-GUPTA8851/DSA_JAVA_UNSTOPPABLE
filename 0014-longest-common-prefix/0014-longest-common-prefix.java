e8c68115c0ea272f0962aed8ff6cc2f8eb7557a2
PREM KUMAR GUPTA <premgupta100705@gmail.com>
2026-08-13 00:05:30 +0530

    Time: 0 ms (100%), Space: 43.4 MB (16.87%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -1,17 +1,27 @@
 class Solution {
     public String longestCommonPrefix(String[] strs) {
+// strs = ["interview", "internet", "internal", "into"] --> o/p --> int
+    String prefix = strs[0];
+    // prefix = interview // first word pkda 
+    // Pehli string ko baaki sab strings se compare karenge
+    for (int i = 1; i < strs.length; i++) {
 
-        String prefix = strs[0];
+    // Jab tak current string prefix se start nahi hoti,
+    // prefix ka last character remove karte jao
+    while (!strs[i].startsWith(prefix)) {
+        // internet.startswith(interview) // false
+        // internet.startswith(intervie) // false
+        // internet.startswith(intervi) // false
+        // internet.startswith(interv) // false
+        // internet.startswith(inter) // True
 
-        // Pehli string ko baaki sab strings se compare karenge
-        for (int i = 1; i < strs.length; i++) {
-
-            // Jab tak current string prefix se start nahi hoti,
-            // prefix ka last character remove karte jao
-            while (!strs[i].startsWith(prefix)) {
-                prefix = prefix.substring(0, prefix.length() - 1);
-            }
-        }
+        // internal.startswith(inter) // true
+        // into.startswith(inter) // 
+        prefix = prefix.substring(0, prefix.length() - 1);
+        // prefix = interview.substring(0, 8) // intervie
+        
+    }
+}
 
         return prefix;
     }
