ef8e9ffd6a9262f85e2a03c94fccf13d6b6c4a29
PREM KUMAR GUPTA <premgupta100705@gmail.com>
2026-08-12 22:53:17 +0530

    Time: 3 ms (77.88%), Space: 46.1 MB (93.97%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.java
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -1,31 +1,28 @@
 class Solution {
     public int romanToInt(String s) {
-
         int ans = 0;
-
-        for (int i = 0; i < s.length(); i++) {
-
-            int curr = value(s.charAt(i));
-
-            // Agar current chhota hai next se → subtract
-            if (i + 1 < s.length() && curr < value(s.charAt(i + 1))) {
-                ans -= curr;
-            } 
-            else {
-                ans += curr;
-            }
+// MCMXCIV
+        for(int i = 0; i < s.length(); i++){
+            char left = s.charAt(i);
+            // left = L 
+        // main logic is left wala chota ho right wale se to ans m subtract kr denge 
+        if(i + 1 < s.length() && value(left) < value(s.charAt(i + 1)))
+        ans -= value(left);
+        // aur agar aesa nhi h (right wala chota h left wale se to ans m add kr denge)
+        else 
+            ans += value(left);
         }
-
         return ans;
     }
+    int value(char ch){
+            if(ch == 'I') return 1;
+            if(ch == 'V') return 5;
+            if(ch == 'X') return 10;
+            if(ch == 'L') return 50;
+            if(ch == 'C') return 100;
+            if(ch == 'D') return 500;
+            if(ch == 'M') return 1000;
 
-    int value(char ch) {
-        if (ch == 'I') return 1;
-        if (ch == 'V') return 5;
-        if (ch == 'X') return 10;
-        if (ch == 'L') return 50;
-        if (ch == 'C') return 100;
-        if (ch == 'D') return 500;
-        return 1000; // M
+        return 0;
     }
 }
