1c5e07cefdc277e486057525a65f2a302918ea4c
PREM KUMAR GUPTA <premgupta100705@gmail.com>
2026-08-15 10:22:39 +0530

    Time: 3 ms (29.82%), Space: 49.1 MB (10.36%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -1,16 +1,16 @@
 class Solution {
     public List<String> letterCombinations(String digits) {
-        List<String> ans = new ArrayList<>();
+    List<String> ans = new ArrayList<>();
 
-        if (digits.length() == 0)
-            return ans;
+    if (digits.length() == 0)
+        return ans;
 
-        String[] map = {
-                "", "", "abc", "def", "ghi",
-                "jkl", "mno", "pqrs", "tuv", "wxyz"
-        };
+    String[] map = {
+            "", "", "abc", "def", "ghi",
+            "jkl", "mno", "pqrs", "tuv", "wxyz"
+    };
 
-        solve(0, digits, map, "", ans);
+    solve(0, digits, map, "", ans);
 
         return ans;
     }
@@ -18,18 +18,18 @@ class Solution {
     void solve(int index, String digits, String[] map,
             String current, List<String> ans) {
 
-        if (index == digits.length()) {
-            ans.add(current);
-            return;
+    if (index == digits.length()) {
+        ans.add(current);
+        return;
         }
 
         int digit = digits.charAt(index) - '0';
 
         String letters = map[digit];
 
-        for (int i = 0; i < letters.length(); i++) {
-            solve(index + 1, digits, map,
-                    current + letters.charAt(i), ans);
+    for (int i = 0; i < letters.length(); i++) {
+        solve(index + 1, digits, map,
+            current + letters.charAt(i), ans);
         }
     }
 }
