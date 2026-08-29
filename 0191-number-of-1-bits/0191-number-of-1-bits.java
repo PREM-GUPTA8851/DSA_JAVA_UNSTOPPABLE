6902b5680f058a745e82159fbb2168264a8ba41d
PREM KUMAR GUPTA <premgupta100705@gmail.com>
2026-08-29 16:17:29 +0530

    Time: 0 ms (100%), Space: 42.6 MB (11.71%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.java
+++ 0191-number-of-1-bits/0191-number-of-1-bits.java
@@ -3,14 +3,19 @@ class Solution {
         // 6 -> 
         int count = 0;
     // 6 > 0
-    // 8 > 0
+    // 8 > 0    
+    // 11 > 0
         while(n > 0) {
             n = n & (n - 1); // rightmost 1 ko remove
         // n = 110 & 101 --> 100 --> 4
         // n = 100 & 011 --> 0
+
+        // n = 1000 & 0111 --> 0
             count++;
         // c =1
         // c =2
+
+        // c= 1
         }
 
         return count;  
