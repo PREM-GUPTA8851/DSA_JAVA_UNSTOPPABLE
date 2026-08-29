ff448c67d4354902c3e8d25c31b8a0295fefe289
PREM KUMAR GUPTA <premgupta100705@gmail.com>
2026-08-29 16:14:25 +0530

    Time: 0 ms (100%), Space: 42.3 MB (65.84%) - LeetHub

---
+++ 0191-number-of-1-bits/0191-number-of-1-bits.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int hammingWeight(int n) {
+        // 6 -> 
+        int count = 0;
+    // 6 > 0
+    // 8 > 0
+        while(n > 0) {
+            n = n & (n - 1); // rightmost 1 ko remove
+        // n = 110 & 101 --> 100 --> 4
+        // n = 100 & 011 --> 0
+            count++;
+        // c =1
+        // c =2
+        }
+
+        return count;  
+    }
+}
