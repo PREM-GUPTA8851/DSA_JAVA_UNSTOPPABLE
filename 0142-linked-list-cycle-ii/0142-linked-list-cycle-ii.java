0970b75387b84679b32e355c975b9c1d95c7c58f
PREM KUMAR GUPTA <premgupta100705@gmail.com>
2026-09-02 23:12:01 +0530

    Time: 0 ms (100%), Space: 46.7 MB (30.54%) - LeetHub

---
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
@@ -0,0 +1,51 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+
+        ListNode slow = head;
+        ListNode fast = head;
+        // slow 1 step chalega
+        // fast 2 step chalega
+
+        while (fast != null && fast.next != null) {
+
+            slow = slow.next;
+            fast = fast.next.next;
+
+            // Example:
+            // 3 → 2 → 0 → -4
+            //          ↑     ↓
+            //          ← ← ←
+
+            // slow aur fast ek point par mil gaye
+            // matlab cycle present hai
+
+            if (slow == fast) {
+
+                slow = head;
+                // ek pointer head par wapas
+
+                while (slow != fast) {
+                    slow = slow.next;
+                    fast = fast.next;
+                }
+
+                return slow;
+                // yahi cycle ka starting node hai
+            }
+        }
+
+        return null;
+        // cycle nahi hai
+    }
+}
