b34aad0aca90730af852f819a21916df0b3c24b5
PREM KUMAR GUPTA <premgupta100705@gmail.com>
2026-09-11 00:09:35 +0530

    Time: 0 ms (100%), Space: 44.4 MB (33.5%) - LeetHub

--- 1441-build-an-array-with-stack-operations/1441-build-an-array-with-stack-operations.java
+++ 1441-build-an-array-with-stack-operations/1441-build-an-array-with-stack-operations.java
@@ -1,25 +1,23 @@
 class Solution {
     public List<String> buildArray(int[] target, int n) {
-
-        List<String> ans = new ArrayList<>();
-
-        int j = 0;
-        // target ko match karne ke liye pointer
-
-        for (int i = 1; i <= n && j < target.length; i++) {
-
-            ans.add("Push");
-            // har number pe pehle Push hoga
-
-            if (i == target[j]) {
-                j++;
-                // target me mila → Push rehne do
-            } else {
-                ans.add("Pop");
-                // target me nahi mila → Push karke turant Pop
-            }
+    List<String> ans = new ArrayList<>();
+    
+    int j = 0;  // y target array ko point out krega
+    // target = [1, 3] , n = 3--> 1, 2, 3
+    // stream m aapn number lenge aur push krte rhenge 
+    for(int i = 1; i <= n && j < target.length; i++){
+        ans.add("Push");// ["Push"]
+        if(i == target[j]){
+            // element mil gya 
+            // 1 == 1 
+            j++;// next target element find krenge
         }
-
-        return ans;
+        else {
+            // if n mila tb 
+            // 2 != 3
+            ans.add("Pop");
+        }
+    }
+    return ans;
     }
 }
