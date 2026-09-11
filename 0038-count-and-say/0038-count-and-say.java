6949a164ed2747bf79686843863aa066d8117d21
PREM KUMAR GUPTA <premgupta100705@gmail.com>
2026-09-12 00:30:49 +0530

    Time: 8 ms (60.57%), Space: 43.2 MB (39.86%) - LeetHub

--- 0038-count-and-say/0038-count-and-say.java
+++ 0038-count-and-say/0038-count-and-say.java
@@ -2,65 +2,40 @@ class Solution {
     public String countAndSay(int n) {
         String ans = "1"; // Base case
         // Generate sequence from 2 to n
-        // 2,3,4,5
+        // 2,3,4
         for (int i = 2; i <= n; i++) {
         // i = 2
         // i = 3
         // i = 4
-        // i = 5
-        StringBuilder temp = new StringBuilder();
-        int count = 1;
-        // ans --> 1 --> length = 1
-        // ans --> 11 --> length = 2
-        // ans --> 21 --> length = 2
-        // ans --> 1211 --> length = 4
+            StringBuilder temp = new StringBuilder();
+            // temp = "";
+            int count = 1;
 
+            // Traverse previous string
+            // ans.length = 1
+            // ans.length = 2
 
-        // 2nd one --> 1 < 2 --> ..
-        // 3rd one --> 1 < 2-->...
-        // 4th one --> 1 < 4
     for (int j = 1; j < ans.length(); j++) {
-        // 4th one --> 1 < 4
-        // 2 < 4
-        // 3 < 4
-        if(ans.charAt(j) == ans.charAt(j - 1)) {
-            count++; 
-            // 2
-        // count = 2
-        } else {
-                    temp.append(count); 
-                    // temp.append(1) --> temp --> 1
-                    // temp.append(1);
-                    // temp.append(1);
-                    temp.append(ans.charAt(j - 1)); 
-                    // temp.append(ans.charAt(0)) --> 
-                    // temp.append(2) --> temp --> 12
-                    // temp.append(1)
-                    // temp.append(2)
 
-                    /// temp --> 1112
+        if(ans.charAt(j) == ans.charAt(j - 1)) {
+            count++; // count = 2
+                } else {
+                    temp.append(count); 
+                    temp.append(ans.charAt(j - 1)); 
                     count = 1;
                 }
             }
 
             // Last group
             temp.append(count);
-            // temp.append(1) --> temp --> 1
-            // temp.append(2) --> 2
-            // temp.append(1) --> 121
-            // temp.append(2)
+            // temp = 1
+            // temp.append(2);
             temp.append(ans.charAt(ans.length() - 1));
-            // temp.append(ans.charAt(0)) --> temp.append(1)
-            // temp.append(ans.charAt(1)) --> temp.append(1)
-            // temp.append(1) --> temp --> 1211
+            // 11
             // temp.append(1)
-
-            // temp--> 111211
             ans = temp.toString();
-            // ans = "11"
-            // ans = "21"
-            // ans = "111211"
-
+            // ans = 11
+            // ans = 21
         }
 
         return ans;
